841451572a75a057729d567837c4900451e9da40
Rafael Fakhreev <fahreevrafel@yandex.ru>
2017-06-20 11:54:14 +0300

    Init

--- logs/loganylazer.py
+++ logs/loganylazer.py
@@ -7,14 +7,14 @@ def anylaze_file(path):
         for line in f:
             [_, _, _, type, system_time, logic_time, _] = line.split()
             system_time, logic_time = float(system_time), int(logic_time)
-            assert system_time >= prev_system_time, 'system time'
-            assert logic_time >= prev_logic_time, 'log time'
+            assert system_time >= prev_system_time
+            assert logic_time >= prev_logic_time
             if type == 'request':
-                assert prev_type == 'request' or prev_type == 'release', 'action'
+                assert prev_type == 'request' or prev_type == 'release'
             elif type == 'acquire':
-                assert prev_type == 'request', 'action'
+                assert prev_type == 'request'
             elif type == 'release':
-                assert prev_type == 'acquire', 'action'
+                assert prev_type == 'acquire'
             prev_type, prev_system_time, prev_logic_time = type, system_time, logic_time
         else:
             print('Ok')
